d421290cf2c9110200976a3f032f90518d129580
Kathryn Barker <270022145@cit3-501m-10.local>
2019-07-26 14:52:03 +1200

    can you make todo app step 1 to 10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.watchlist = [
+      {
+        id:1,
+        content:'The Godfather 1',
+        genre: 'Action'
+      },{
+        id:2,
+        content:'The Godfather 2',
+        genre: 'Action'
+      },{
+        id:3,
+        content:'The Godfather 3',
+        genre: 'Action'
+      }];
+
+  }
+  render(){
+    return(
+      <div className="wrap">
+        <div className="container">
+          <div className="card mx-auto shadow-lg " >
+            <div className="card-body">
+                  <h2 className="card-title ">Movies to Watch</h2>
+                  <p className="class-text">Add movies to your watchlist</p>
+                  <ul className="list-group list-group-flush movie-list">
+                    {
+                      this.watchlist.map(function(movie){
+                        return (
+                          <li className="list-group-item movie-list-item" key={movie.id}>
+                            <div className="movie-title">{movie.content}</div>
+                          <i className="far fa-circle"></i>
+                          <div className="genre">
+                              <p><small>{movie.genre}</small></p>
+                          </div>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+              </div>
+            <form className="card-body ">
+                  <div className="form-row">
+                          <label className="col-12"for="movie-input">New Movie</label>
+                  </div>
+        <div className="form-row align-items-center">
+                      <div className="form-group col-auto">
+                          <input type="text" className="form-control form-control-sm" id="movie-input"/>
+                      </div>
+                      <div className="form-group col-auto">
+                          <button type="submit" className="btn btn-primary"><i className="fas fa-plus-circle"></i></button>
+                      </div>
+                  </div>
+              </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
