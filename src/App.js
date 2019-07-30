cefec30ca8b1e3806658735c66f06e097ebceef2
Kathryn Barker <270022145@cit3-501m-10.local>
2019-07-30 15:53:14 +1200

    added tick boxes and add and delete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,84 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.watchlist = [
-      {
-        id:1,
-        content:'The Godfather 1',
-        genre: 'Action'
-      },{
-        id:2,
-        content:'The Godfather 2',
-        genre: 'Action'
-      },{
-        id:3,
-        content:'The Godfather 3',
-        genre: 'Action'
-      }];
+    this.state = {
+      movies: [
+        {
+          id:1,
+          content:'The Godfather 1',
+          genre: 'Action',
+          status:'new'
+        },{
+          id:2,
+          content:'The Godfather 2',
+          genre: 'Action',
+          status:'new'
+        },{
+          id:3,
+          content:'The Godfather 3',
+          genre: 'Action',
+          status:'new'
+        }
+      ],
+      movieTitleInputValue: '',
+      genreInputValue: '',
+    };
 
   }
+
+  handleMovieTitleInputChange = (e) =>{
+    this.setState({movieTitleInputValue: e.target.value});
+
+  };
+  handleGenreInputChange = (e) => {
+    this.setState({genreInputValue: e.target.value});
+    
+  };
+
+  handleAddMovieClick = (e) => {
+    e.preventDefault();
+
+    var movie = {
+      id: Date.now(),
+      content: this.state.movieTitleInputValue,
+      genre: this.state.genreInputValue
+    };
+
+    var newMovie = [...this.state.movies,movie];
+  
+    this.setState({
+      movies: newMovie,
+      movieTitleInputValue: '',
+      genreInputValue: '',
+    });
+  };
+
+  removeMovie = (movieId) => {
+    var movies = this.state.movies;
+    var filteredMovies = movies.filter( function(movie){
+      return movie.id !== movieId;
+    });
+    // console.log(this);
+    this.setState({movies:filteredMovies});
+  };
+
+  updateStatus = (id,status) => {
+    var movies = this.state.movies;
+    var index = movies.findIndex(function(movie){
+      return movie.id === id;
+    });
+
+    movies[index].status = status;
+  
+    this.setState({movies});
+    
+  }
+
   render(){
+
     return(
       <div className="wrap">
         <div className="container">
@@ -31,11 +88,16 @@ class App extends Component {
                   <p className="class-text">Add movies to your watchlist</p>
                   <ul className="list-group list-group-flush movie-list">
                     {
-                      this.watchlist.map(function(movie){
+                      this.state.movies.map((movie) => {
+                        
                         return (
                           <li className="list-group-item movie-list-item" key={movie.id}>
                             <div className="movie-title">{movie.content}</div>
-                          <i className="far fa-circle"></i>
+
+                            {
+                              (movie.status==='new') ? <i onClick={(e)=>{this.updateStatus(movie.id,'watched')}} className="far fa-circle"></i> : <i onClick={(e)=> {this.updateStatus(movie.id,'new')}} className="far fa-check-circle"></i>
+                            }
+                          <i className="far fa-times-circle" onClick={ (e) => {this.removeMovie(movie.id)}}></i>
                           <div className="genre">
                               <p><small>{movie.genre}</small></p>
                           </div>
@@ -46,16 +108,20 @@ class App extends Component {
                   </ul>
               </div>
             <form className="card-body ">
-                  <div className="form-row">
-                          <label className="col-12"for="movie-input">New Movie</label>
+                  <div className="form-row align-items-center">
+                      <div className="form-group col-12">
+                          <input type="text" className="form-control form-control-sm" id="movie-input" placeholder="Movie Title" onChange={this.handleMovieTitleInputChange}/>
+                      </div>
                   </div>
-        <div className="form-row align-items-center">
-                      <div className="form-group col-auto">
-                          <input type="text" className="form-control form-control-sm" id="movie-input"/>
-                      </div>
-                      <div className="form-group col-auto">
-                          <button type="submit" className="btn btn-primary"><i className="fas fa-plus-circle"></i></button>
-                      </div>
+                  <div className="form-row align-items-center">
+                    <div className="form-group col-12">
+                      <input type="text" className="form-control form-control-sm" id="genre-input" placeholder="Genre" onChange={this.handleGenreInputChange}/>
+                    </div>
+                  </div>
+                  <div className="form-row align-items-center">
+                    <div className="form-group col-12">
+                      <button type="submit" className="btn btn-primary col-12" onClick={this.handleAddMovieClick}><i className="fas fa-plus-circle" ></i></button>
+                    </div>
                   </div>
               </form>
           </div>
